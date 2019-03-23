5a561f2c6f510dbb733e45478b535a7f6299d882
Wade HSU <fatooo0116@gmail.com>
2019-03-23 17:36:03 +0800

    update

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+export default class BurgerBuilder extends Component{
+
+  render(){
+    return(
+      <Aux>
+        <div>Burger</div>
+        <div>Build</div>
+      </Aux>
+    );
+  }
+}
